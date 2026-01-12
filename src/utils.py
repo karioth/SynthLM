820c1685249052986d28e029b7536a167a152037
karioth <friveros.segovia@gmail.com>
2026-01-12 15:11:13 +0100

    deleted legacy HF

--- src/utils.py
+++ src/utils.py
@@ -1,65 +1,13 @@
 from PIL import Image
 import numpy as np
 import json
-from collections import OrderedDict
 import torch
-import torch.distributed as dist
-import logging
 import os
 import requests
 from tqdm import tqdm
 from lightning.pytorch.callbacks import WeightAveraging
 from torch.optim.swa_utils import get_ema_avg_fn
 from .tokenizer_models import AutoencoderKL, sigma_vae
-
-#################################################################################
-#                             Training Helper Functions                         #
-#################################################################################
-
-@torch.no_grad()
-def update_ema(ema_model, model, decay=0.9999):
-    """
-    Step the EMA model towards the current model.
-    """
-    ema_params = OrderedDict(ema_model.named_parameters())
-    model_params = OrderedDict(model.named_parameters())
-
-    for name, param in model_params.items():
-        # TODO: Consider applying only to params that require_grad to avoid small numerical changes of pos_embed
-        ema_params[name].mul_(decay).add_(param.data, alpha=1 - decay)
-
-
-def requires_grad(model, flag=True):
-    """
-    Set requires_grad flag for all parameters in a model.
-    """
-    for p in model.parameters():
-        p.requires_grad = flag
-
-
-def cleanup():
-    """
-    End DDP training.
-    """
-    dist.destroy_process_group()
-
-
-def create_logger(logging_dir):
-    """
-    Create a logger that writes to a log file and stdout.
-    """
-    if dist.get_rank() == 0:  # real logger
-        logging.basicConfig(
-            level=logging.INFO,
-            format='[\033[34m%(asctime)s\033[0m] %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S',
-            handlers=[logging.StreamHandler(), logging.FileHandler(f"{logging_dir}/log.txt")]
-        )
-        logger = logging.getLogger(__name__)
-    else:  # dummy logger (does nothing)
-        logger = logging.getLogger(__name__)
-        logger.addHandler(logging.NullHandler())
-    return logger
 
 def center_crop_arr(pil_image, image_size):
     """
